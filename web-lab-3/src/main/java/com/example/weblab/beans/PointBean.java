73a61e8c1ccf99200ee48f0ce2799378dbdfcbb5
undndnwnkk <bykov.lev74@gmail.com>
2025-11-13 21:48:00 +0300

    refactored point bean

--- web-lab-3/src/main/java/com/example/weblab/beans/PointBean.java
+++ web-lab-3/src/main/java/com/example/weblab/beans/PointBean.java
@@ -5,13 +5,15 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Data;
 
-import javax.faces.view.ViewScoped;
+import jakarta.faces.view.ViewScoped;
+
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Named
 @ViewScoped
 @Data
-public class PointBean {
+public class PointBean implements Serializable {
     private Double x;
     private Double y;
     private Double r;
@@ -22,15 +24,17 @@ public class PointBean {
     public void checkHit() {
         long startTime = System.nanoTime();
         boolean isHit = true;
-        if (x > 0 && y > 0) {
-            isHit = (x <= r / 2) && (y <= r / 2) && (x*x + y*y <= (r/2) * (r/2));
-        } else if(x < 0 && y < 0) {
-            isHit = (x >= -r) && (y >= -r) && (y >= -2 * x - r);
-        } else if (x > 0 && y < 0) {
-            isHit = (x <= r) && (y >= -r);
-        } else {
-            isHit = false;
+
+        if (x <= 0 && y >= 0) {
+            isHit = (y <= (x / 2 + r / 2));
         }
+        else if (x <= 0 && y <= 0) {
+            isHit = (x >= -r) && (y >= -r);
+        }
+        else if (x >= 0 && y <= 0) {
+            isHit = (x * x + y * y <= (r / 2) * (r / 2));
+        }
+
         long executionTime = System.nanoTime() - startTime;
 
         CheckResult checkResult = CheckResult.builder()
