218d556b962607d45eb6d0853cf34b1ad22c6d68
undndnwnkk <bykov.lev74@gmail.com>
2026-02-26 14:42:13 +0300

    passed task num 0 algs

--- algorithms-2nd-course/square_string.cpp
+++ algorithms-2nd-course/square_string.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::string lines[n];
+  int n;
+  std::cin >> n;
+  std::vector<std::string> lines(n);
 
-    for (int i = 0; i < n; i++) {
-        std::string str;
-        std::cin >> str;
-        lines[i] = str;
+  for (int i = 0; i < n; i++) {
+    std::string str;
+    std::cin >> str;
+    lines[i] = str;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (lines[i].size() % 2 != 0) {
+      std::cout << "NO" << std::endl;
+      continue;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (lines[i].size() % 2 != 0) {
-            std::cout << "NO" << std::endl;
-            continue;
-        }
+    int half = lines[i].size() / 2;
+    bool is_true = true;
 
-        int half = lines[i].size() / 2;
-        bool is_true = true;
-
-        for (int l = 0; l < half; l++) {
-            if (lines[i][l] != lines[i][l + half]) {
-                is_true = false;
-                break;
-            }
-        }
-
-        if (is_true) {
-            std::cout << "YES" << std::endl;
-        } else {
-            std::cout << "NO" << std::endl;
-        }
+    for (int l = 0; l < half; l++) {
+      if (lines[i][l] != lines[i][l + half]) {
+        is_true = false;
+        break;
+      }
     }
 
-    return 0;
+    if (is_true) {
+      std::cout << "YES" << std::endl;
+    } else {
+      std::cout << "NO" << std::endl;
+    }
+  }
+
+  return 0;
 }
